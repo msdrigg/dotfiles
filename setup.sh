cdb1231f98cfb5f72bb4143308187cb0d86a6c86
Scott Driggers <m.scott.driggers@gmail.com>
2021-05-21 15:16:02 -0400

    Testing symlink in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
-
 cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
+
+# We still need this.
+windows() { [[ -n "$WINDIR" ]]; }
+
+# Cross-platform symlink function. With one parameter, it will check
+# whether the parameter is a symlink. With two parameters, it will create
+# a symlink to a file or directory, with syntax: link $linkname $target
+link() {
+    if [[ -z "$2" ]]; then
+        # Link-checking mode.
+        if windows; then
+            fsutil reparsepoint query "$1" > /dev/null
+        else
+            [[ -h "$1" ]]
+        fi
+    else
+        # Link-creation mode.
+        if windows; then
+            # Windows needs to be told if it's a directory or not. Infer that.
+            # Also: note that we convert `/` to `\`. In this case it's necessary.
+            if [[ -d "$2" ]]; then
+                cmd <<< "mklink /D \"$1\" \"${2//\//\\}\"" > /dev/null
+            else
+                cmd <<< "mklink \"$1\" \"${2//\//\\}\"" > /dev/null
+            fi
+        else
+            # You know what? I think ln's parameters are backwards.
+            ln -s "$2" "$1"
+        fi
+    fi
+}
 
 function doIt() {
 	# Common aliases
@@ -36,6 +66,10 @@ function doIt() {
 		echo "Running local setup script at $LOCALSETUP"
 		source "$LOCALSETUP";
 	fi
+
+	echo "Setting up link to diff-so-fancy"
+	link "$HOME/bin/diff-so-fancy" "diff-so-fancy/diff-so-fancy"
+
 	source ~/.profile;
 }
 
