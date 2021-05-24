8de7e19eabb3be059ac6ceeedafb25e85f81d53d
Scott Driggers <m.scott.driggers@gmail.com>
2021-05-24 01:37:47 -0400

    Fixing issue with variable in setup_linux

--- setup_linux.sh
+++ setup_linux.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-#!/bin/bash
 set -e
-SYSTEM_NAME = "linux"
+
+SYSTEM_NAME="linux"
 if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
-    SYSTEM_NAME = wsl
+    SYSTEM_NAME="wsl"
 fi
 
 source setup_common.sh "~" "$SYSTEM_NAME"
