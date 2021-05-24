0d0b742f235b0a0e69cb6611ba7d88e5f1ce8572
Scott Driggers <m.scott.driggers@gmail.com>
2021-05-24 02:21:24 -0400

    Error in setup_common.sh

--- setup_common.sh
+++ setup_common.sh
@@ -11,19 +11,22 @@ LOCAL=$2
 
 function syncDirectories() {
 	if command -v rsync &> /dev/null; then
-	  rsync -avh --no-perms $1/. $2;
+	  rsync -ah --no-perms $1/. $2;
 	else
 	  cp -a $1/. $2;
 	fi
 }
 
+echo "Syncing common folder"
 syncDirectories $DOTFILES_HOME/common $TARGET_HOME
+echo "Syncing $LOCAL folder"
 syncDirectories $DOTFILES_HOME/$LOCAL $TARGET_HOME
 
 BASHSCRIPT="$DOTFILES_HOME/local/$LOCAL/.bash_local"
 if [ -f $BASHSCRIPT ]; then
 	echo "Copying local bash script from $BASHSCRIPT"
 	cp "$BASHSCRIPT" $TARGET_HOME/.bash_local;
+	source $TARGET_HOME/.bash_local
 fi
 LOCALSETUP="$DOTFILES_HOME/local/$LOCAL/setup.sh"
 if [ -f $LOCALSETUP ]; then
@@ -31,7 +34,7 @@ if [ -f $LOCALSETUP ]; then
 	source "$LOCALSETUP";
 fi
 
-echo "Setting up path to diff-so-fancy"
+echo "Setting up path to diff-so-fancy in $TARGET_HOME/bin"
 mkdir -p $TARGET_HOME/bin/lib
 syncDirectories "$DOTFILES_HOME/diff-so-fancy/lib" "$TARGET_HOME/bin/lib"
 cp "$DOTFILES_HOME/diff-so-fancy/diff-so-fancy" "$TARGET_HOME/bin"
