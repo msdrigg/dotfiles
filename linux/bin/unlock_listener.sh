68eabf8194560479a997fc269eb4f73843dfbc05
Scott Driggers <m.scott.driggers@gmail.com>
2021-06-26 09:40:05 -0400

    Adding screen configurations

--- linux/bin/unlock_listener.sh
+++ linux/bin/unlock_listener.sh
@@ -2,7 +2,7 @@
 
 dbus-monitor --session "type='signal', interface='org.gnome.ScreenSaver'" |
     while read x; do
-        if [ "$x" == "boolean false" ] && ! test_dual_monitors.sh; then
-            configure_dual_monitors.sh;
+        if [ "$x" == "boolean false" ] && ! test_dual_monitors; then
+            configure_dual_monitors;
         fi
     done
